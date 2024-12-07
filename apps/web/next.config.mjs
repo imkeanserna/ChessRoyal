a46a8bd9cc4375f6680b38fd8fe3a2f1b25b6a97
imkurosaki <keandelaserna11@gmail.com>
2024-12-07 09:58:57 +0800

    fix: Change the package.json for vercel

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       'yourdomainname.com'
     ]
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
